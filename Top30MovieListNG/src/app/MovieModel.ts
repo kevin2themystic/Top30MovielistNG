ffc543ee3feb71a84bc81c1a89ed656ac13e0afd
Kevin Parkinson <kevin.parkinson@rosenau.ca>
2017-09-20 15:21:22 -0600

    Added VS solution and MovieModel

---
+++ Top30MovieListNG/src/app/MovieModel.ts
@@ -0,0 +1,23 @@
+/*
+
+*/
+export class Movie {
+    SequenceNumber: number;
+    MovieName: string;
+    Notes: string;
+    MovieParts: Array<MoviePart> = new Array<MoviePart>();
+    IMDBUrl: string;
+    IMDBPosterUrl: string;
+
+    constructor() {
+
+    }     
+}
+/*
+
+*/
+export class MoviePart {
+    MovieName: string;
+    Notes: string;
+    IMDBUrl: string;
+}
--- Top30MovieListNG/src/app/app.component.ts
+++ Top30MovieListNG/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Movie } from './MovieModel';
+import { MoviePart } from './MovieModel';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-    title = 'app';                                        
+export class AppComponent { 
+    Movies: Array<Movie> = new Array<Movie>();
+
+    constructor() {
+
+    }
 }
